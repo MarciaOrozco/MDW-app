9f00c8bdf00db573b834fe5545c3ee3e656ff4f9
MarciaOrozco <orozcomarcia3@gmail.com>
2025-01-25 17:35:32 -0300

    modelo de book

---
+++ src/types/books.ts
@@ -0,0 +1,11 @@
+export interface Book {
+  _id: number;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  author: string;
+  isbn: string;
+  createdAt: string;
+  updatedAt: string;
+}
