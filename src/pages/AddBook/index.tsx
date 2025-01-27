baa699c3e4bf7a1f7a1a87fadb67484dcfcaf197
MarciaOrozco <orozcomarcia3@gmail.com>
2025-01-27 14:10:39 -0300

    formulario para la creacion de un nuevo libro

---
+++ src/pages/AddBook/index.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../../config/axios";
+
+const AddBookPage: React.FC = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    image: "",
+    author: "",
+    isbn: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await api.post("/books", formData);
+      navigate("/books");
+    } catch (error) {
+      console.error("Error adding book:", error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center items-center p-4 font-mono">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-gray-50 text-stone-700 shadow-lg rounded-lg p-8 max-w-lg w-full"
+      >
+        <h2 className="text-2xl font-bold text-gray-800 mb-6">Add new book</h2>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2">
+            Book Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="w-full px-4 py-2 rounded-lg bg-white text-gray-500 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-rose-200"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2">Author</label>
+          <input
+            type="text"
+            name="author"
+            value={formData.author}
+            onChange={handleChange}
+            className="w-full px-4 py-2 rounded-lg bg-white text-gray-500 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-rose-200"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2">
+            Description
+          </label>
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            className="w-full px-4 py-2 rounded-lg bg-white text-gray-500 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-rose-200"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2">Price</label>
+          <input
+            type="number"
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
+            className="w-full px-4 py-2 rounded-lg bg-white text-gray-500 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-rose-200"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2">
+            Image URL
+          </label>
+          <input
+            type="text"
+            name="image"
+            value={formData.image}
+            onChange={handleChange}
+            className="w-full px-4 py-2 rounded-lg bg-white text-gray-500 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-rose-200"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2">ISBN</label>
+          <input
+            type="text"
+            name="isbn"
+            value={formData.isbn}
+            onChange={handleChange}
+            className="w-full px-4 py-2 rounded-lg bg-white text-gray-500 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-rose-200"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full hover:bg-[#d1919b]  bg-[#b9757f] text-white font-bold py-2 px-4 rounded-lg transition duration-300 "
+        >
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddBookPage;
